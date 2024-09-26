55378728f8489b853c5698aab56c3acc5412cab4
skramer <skramer5@luc.edu>
2024-09-25 22:45:17 -0500

    Added command line argument parsing

--- build.sbt
+++ build.sbt
@@ -7,8 +7,10 @@ scalaVersion := "3.3.3"
 scalacOptions += "@.scalacOptions.txt"
 
 libraryDependencies ++= Seq(
+  "com.lihaoyi" %% "mainargs" % "0.6.3",
   "org.scalatest"  %% "scalatest"  % "3.2.19"  % Test,
-  "org.scalacheck" %% "scalacheck" % "1.18.0" % Test
+  "org.scalacheck" %% "scalacheck" % "1.18.0" % Test,
+  "org.apache.commons" % "commons-collections4" % "4.4"
 )
 
 enablePlugins(JavaAppPackaging)
--- src/main/scala/hellotest/Main.scala
+++ src/main/scala/hellotest/Main.scala
@@ -1,9 +1,51 @@
 package hellotest
+import java.util.Scanner
+import mainargs._
+import org.apache.commons.collections4.queue.CircularFifoQueue
 
-object Main:
 
-  def main(args: Array[String]) = 
-    println("Hello scalatest!")
-    println(s"Today's date is ${java.time.LocalDate.now}.")
 
-end Main
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    ParserForMethods(this).runOrExit(args.toIndexedSeq)
+    ()
+  }
+  @main def run(
+           @arg(short = 'c', name = "cloudSize") cloudSize: Int = 10,
+           @arg(short = 'l', name = "minLength") minLength: Int = 6,
+           @arg(short = 'w', name = "windowSize") windowSize: Int = 1000
+         ): Unit = {
+    println(s"cloudSize: $cloudSize, minLength: $minLength, windowSize: $windowSize")
+    queue(cloudSize, minLength, windowSize)
+  }
+}
+  def queue(cloudSize: Int, minLength: Int, windowSize: Int): Unit = {
+    // TODO consider using a command-line option library
+
+    try {
+        if (windowSize < 1) {
+          throw new NumberFormatException()
+        }
+      } catch {
+      case _: NumberFormatException =>
+        Console.err.println("argument should be a natural number")
+        System.exit(4)
+    }
+
+      val input = new Scanner(System.in).useDelimiter("(?U)[^\\p{Alpha}0-9']+")
+      val queue = new CircularFifoQueue(windowSize)
+
+      try {
+        while (input.nn.hasNext) {
+          val word = input.nn.next()
+          queue.add(word) // the oldest item automatically gets evicted
+          Console.println(queue)
+        }
+      } catch {
+        case e: Exception =>
+          Console.err.println(s"An error occurred: ${e.getMessage}")
+          System.exit(1)
+      }
+  }
+
