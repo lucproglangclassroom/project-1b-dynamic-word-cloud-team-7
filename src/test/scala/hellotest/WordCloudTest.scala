032eddf1211a58a23a14e9cdc67aad074c7b21dd
skramer <skramer5@luc.edu>
2024-09-30 22:32:34 -0500

    Changed wording in the definitions of the tests

--- src/test/scala/hellotest/WordCloudTest.scala
+++ src/test/scala/hellotest/WordCloudTest.scala
@@ -10,7 +10,7 @@ import org.scalatest.matchers.should.Matchers
 
 class WordCloudTest extends AnyFlatSpec with Matchers {
 
-  "A word cloud with only 1 unique word" should "have only 1 element" in {
+  "A word cloud with only 1 unique word" should "have a topFrequentWords seq with only 1 element" in {
     val sut = new CircularQueue(6)
     for(a <- 0 to 6) {
       sut.add("asking")
@@ -18,12 +18,12 @@ class WordCloudTest extends AnyFlatSpec with Matchers {
     assert(sut.topFrequentWords(6, 1).length == 1)
   }
 
-  "A word cloud with 0 words" should "have 0 elements" in {
+  "A word cloud with 0 words" should "have a topFrequentWords seq with 0 elements" in {
     val sut = new CircularQueue(6)
     assert(sut.topFrequentWords(6, 1).isEmpty)
   }
 
-  "A word cloud with only unique words and a minFrequency greater than 1" should "have 0 elements" in {
+  "A word cloud with only unique words and a minFrequency greater than 1" should "have a topFrequentWords seq with 0 elements" in {
     val sut = new CircularQueue(6)
     for (a <- 0 to 6) {
       sut.add(a.toString)
